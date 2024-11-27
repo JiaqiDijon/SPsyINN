1ae87c79c9e5397ec57ec0ab6639c3964d63439a
JiaqiDijon <84071205+JiaqiDijon@users.noreply.github.com>
2024-11-27 16:16:32 +0800

    Update run.py

--- trainer/run.py
+++ trainer/run.py
@@ -6,17 +6,17 @@ os_type = platform.system()
 if os_type == "Windows":
     print("Detected Windows OS.")
     process1 = subprocess.Popen(["python", "TorchModel_train.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
-    process2 = subprocess.Popen(["python", "GPSR_train.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    process2 = subprocess.Popen(["python", "GSR_train.py"], creationflags=subprocess.CREATE_NEW_CONSOLE)
 
 elif os_type == "Linux":
     print("Detected Linux OS.")
     process1 = subprocess.Popen(["xterm", "-e", "python TorchModel_train.py"])
-    process2 = subprocess.Popen(["xterm", "-e", "python GPSR_train.py"])
+    process2 = subprocess.Popen(["xterm", "-e", "python GSR_train.py"])
 
 elif os_type == "Darwin":  # macOS
     print("Detected macOS.")
     process1 = subprocess.Popen(["open", "-a", "Terminal", "--args", "python TorchModel_train.py"])
-    process2 = subprocess.Popen(["open", "-a", "Terminal", "--args", "python GPSR_train.py"])
+    process2 = subprocess.Popen(["open", "-a", "Terminal", "--args", "python GSR_train.py"])
 
 else:
     print(f"Unsupported OS: {os_type}")
